2ae313198b684a7ed997f007273e13722aec3658
Simon <simon.saetre@gmail.com>
2021-11-14 23:19:21 +0100

    Added multiple receivers functionality

--- DerpibooruTagAlert.py
+++ DerpibooruTagAlert.py
@@ -12,14 +12,14 @@ port = 587  # For starttls. Don't touch unless you know what you are doing
 smtp_server = "smtp.gmail.com" #Don't touch unless you are not using Gmail.
 
 hostMail = "" #The email sending the email.
-receivingMail = "" #The email you want to receive the email. It can be the same as the host mail but it is smart to have a separate email for scripts.
+receivingMails = [""] #The emails you want to receive the email. It can be the same as the host mail but it is smart to have a separate email for scripts. You can have multiple receiving emails by writing them as ["email1","email2"]. Great if you want to share the alerter with multiple people. You just need their email.
 key = "" #API key at https://derpibooru.org/registrations/edit
 searchFilter = "" #Find the filter ID you want to use at https://derpibooru.org/filters. The ID is the numbers behind the URL. I recommend using "everything" in case you want full freedom. Watch out for unexpected lewd!
 password = "" #The password for your email. Don't worry, I will not and I cannot steal it. I barely know how to program this stuff, let alone a password stealer. Now that I think about it, I can make the program send your info to me pretty easily. DW I haven't done that. Just check the code below lol.
 
 delaytime = 15 #Minutes between each check. DO NOT SET THIS TO A VERY LOW AMOUNT. You can be banned.
 oldPicTolerance = 0.8 #Decrease this value if you experience old pics popping up from time to time. 0.5-0.9 should be fine. Keep the value as high as possible for extremely popular tags. Never set it above 1!!!
-tags = "safe,spitfire" #The tags you want to apply to your search. Separate them by comma. Ex: "Safe, Spitfire, best pony"
+tags = "" #The tags you want to apply to your search. Separate them by comma. Ex: "Safe, Spitfire, best pony"
 
 subject = "New Images!"   #The subject of the message you want to send!
 text = """URL(s):
@@ -72,14 +72,15 @@ while True:
         message = 'Subject: {}\n\n{}'.format(subject, messagetext)#Formats the subject and text into a format understood by the function.
         while True:
             try:
-                context = ssl.create_default_context()
-                with smtplib.SMTP(smtp_server, port) as server:#Magic magic sendy-email-thingy
-                    server.ehlo()
-                    server.starttls(context=context)
-                    server.ehlo()
-                    server.login(hostMail, password)
-                    server.sendmail(hostMail, receivingMail, message)
-                    break
+                for emails in receivingMails:
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP(smtp_server, port) as server:#Magic magic sendy-email-thingy
+                        server.ehlo()
+                        server.starttls(context=context)
+                        server.ehlo()
+                        server.login(hostMail, password)
+                        server.sendmail(hostMail, emails, message)
+                break
             except:
                 print("Something is wrong with the email sender. Check your internet connection or info if everything is correct. Retrying...")
                 time.sleep(60)
